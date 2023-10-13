f1f8db46c61cb0ebc9a34f1f64977b4ee4b6b8be
Trased <117777557+Trased@users.noreply.github.com>
2023-10-13 09:51:21 +0300

    Update main.py

--- Intermediate/Snake Game/main.py
+++ Intermediate/Snake Game/main.py
@@ -1,5 +1,6 @@
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import window
 import time
 
@@ -9,11 +10,13 @@ if __name__ == "__main__":
     snk = Snake()
     food = Food()
     food.change_position()
+    scrb = Scoreboard()
     while game_is_on:
         time.sleep(.1)
+        scrb.print_score()
         scr.update()
         snk.snake_move()
-        snk.detect_collision_with_food(food)
+        snk.detect_collision_with_food(food, scrb)
         snk.detect_collision_with_self()
         snk.snake_change_direction(scr)
         if not snk.get_game():
