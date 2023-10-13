396afed7a0045e3f2c35168eeccca863c6f150cf
Trased <117777557+Trased@users.noreply.github.com>
2023-10-13 09:31:09 +0300

    Update main.py

--- Intermediate/Snake Game/main.py
+++ Intermediate/Snake Game/main.py
@@ -1,46 +1,20 @@
-#Initial commit, changes will be made.
-
+from snake import Snake
+from food import Food
+import window
 import time
-from turtle import Screen, Turtle
-
-
-class Snake:
-    turtles = []
-
-
-def init_screen():
-    screen = Screen()
-    screen.setup(width=600, height=600)
-    screen.bgcolor("black")
-    screen.title("Snake Game")
-    screen.tracer(0)
-    return screen
-
-
-def init_game_snake():
-    turtles = []
-    for i in range(0, 3):
-        turtle = Turtle(shape="square")
-        turtle.color("white")
-        turtle.penup()
-        turtle.setposition(-20 * i, 0)
-        turtles.append(turtle)
-    return turtles
-
-
-def update_positions(turtle, seg_num):
-    return turtle[seg_num].goto(turtle[seg_num-1].xcor(), turtle[seg_num-1].ycor())
-
 
 if __name__ == "__main__":
     game_is_on = True
-    scr = init_screen()
-    trt = init_game_snake()
+    scr = window.init_screen()
+    snk = Snake()
+    food = Food()
+    food.change_position()
     while game_is_on:
+        time.sleep(.1)
         scr.update()
-        for num in range(len(trt) - 1, 0, -1):
-            update_positions(trt, num)
-
-        time.sleep(.2)
-
-    scr.exitonclick()
+        snk.snake_move()
+        snk.detect_collision_with_food(food)
+        snk.detect_collision_with_self()
+        snk.snake_change_direction(scr)
+        if not snk.get_game():
+            scr.exitonclick()
